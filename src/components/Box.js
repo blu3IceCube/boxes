bf6afb49636faa9fb2c0b1e4ba5cdca494bbc1cb
blu3IceCube <111587317+blu3IceCube@users.noreply.github.com>
2022-08-19 21:50:36 +0530

    Add files via upload

---
+++ src/components/Box.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Box(props) {
+
+    const styles = {
+        backgroundColor: props.on ? "#222222" : "#cccccc"
+    }
+    return (
+        <div style={styles} className="box" onClick={props.toggle}></div>
+    )
+}
