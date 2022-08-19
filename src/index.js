d7b7280ab820a5d997a3560c3e8f3563060e0c6a
blu3IceCube <111587317+blu3IceCube@users.noreply.github.com>
2022-08-19 21:49:46 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {createRoot} from "react-dom/client"
+import App from "./App.js"
+
+const rootElement = document.getElementById("root")
+const root = createRoot(rootElement)
+root.render(<App />)
